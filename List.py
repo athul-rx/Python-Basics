42bbb71cc3bc4e5d46d761de7ca1d62f7d532c53
Athul Raj <athulrajclt123@gmail.com>
2023-10-07 09:44:03 +0530

    Added few more concepts

--- List.py
+++ List.py
@@ -12,3 +12,21 @@ print(list1)
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
+
+#list Comprehension, if we want to create a new list based on the existing list
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist) 
+
+#sort a list
+number = [100, 50, 65, 82, 23]
+number.sort()
+print(number)
+#sorting in descending order
+number.sort(reverse=True)
+print(number)
+
+#reverse the order of elements
+fruit = ["banana", "Orange", "Kiwi", "cherry"]
+fruit.reverse()
+print(fruit)
