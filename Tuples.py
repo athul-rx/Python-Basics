baf8fa659b85d4e721d0afe9b3ab49db34e2b1d9
Athul Raj <athulrajclt123@gmail.com>
2023-10-08 12:02:14 +0530

    Basics of tuples

---
+++ Tuples.py
@@ -0,0 +1,6 @@
+#A tuple is a collection which is ordered and unchangeable.
+#It allows duplicate elements, just like list
+
+#Accessing Tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
